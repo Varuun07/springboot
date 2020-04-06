98c3550960ee3f8a0f51513683d3ae802e451e83
Joel Krooswyk <jkrooswyk@gitlab.com>
2020-04-06 21:02:27 +0000

    change screen representation

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,9 +9,17 @@ import org.springframework.web.bind.annotation.*;
 public class DemoApplication {
 
 	@GetMapping("/")
-	String home() {
-		return "Spring is here!";
-	}
+    public String index() {
+        String style = "<style type='text/css' media='screen'>";
+        style += "body { background-color: purple; position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); color: orange; font-size: 250%; }";
+        style += "</style>";
+        
+        String message = "Spring is here!";
+        
+        String body = "<body>" + message + "</body>";
+
+        return style + body;
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
